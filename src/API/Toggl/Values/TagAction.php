ece6853f71f64b9dd32d68f178afaae127dee55f
Mario Blazek <mario.b@netgen.hr>
2016-04-08 08:35:37 +0200

    TagAction class

---
+++ src/API/Toggl/Values/TagAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Marek\Toggable\API\Toggl\Values;
+
+/**
+ * Class TagAction
+ * @package Marek\Toggable\API\Toggl\Values
+ */
+class TagAction
+{
+    const ADD = 'add';
+
+    const REMOVE = 'remove';
+}
