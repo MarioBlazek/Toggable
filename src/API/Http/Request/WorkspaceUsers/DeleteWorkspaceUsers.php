f7f85fe646e9642d8bc8e2880a16df26456a1d7b
Mario Blazek <mario.b@netgen.hr>
2016-04-08 08:32:36 +0200

    WorkspaceUsers request value objects

---
+++ src/API/Http/Request/WorkspaceUsers/DeleteWorkspaceUsers.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Marek\Toggable\API\Http\Request\WorkspaceUsers;
+
+use Marek\Toggable\API\Http\Request\Request;
+
+class DeleteWorkspaceUsers extends Request
+{
+
+}
---
+++ src/API/Http/Request/WorkspaceUsers/InviteUserToWorkspace.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Marek\Toggable\API\Http\Request\WorkspaceUsers;
+
+use Marek\Toggable\API\Http\Request\Request;
+
+class InviteUserToWorkspace extends Request
+{
+
+}
