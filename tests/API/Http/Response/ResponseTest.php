0ac0497092318d6bbaecc745717f3700b0b7c893
Mario Blazek <mario.b@netgen.hr>
2016-04-24 09:03:18 +0200

    Response test

---
+++ tests/API/Http/Response/ResponseTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Marek\Toggable\Tests\API\Http\Response;
+
+use Marek\Toggable\API\Http\Response\Response;
+
+class ResponseTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInstanceOf()
+    {
+        $request = new Response();
+        $this->assertInstanceOf('\Marek\Toggable\API\Http\Response\ResponseInterface', $request);
+    }
+
+    public function testGetBody()
+    {
+        $response = new Response(array('body' => 'body'));
+        $this->assertEquals('body', $response->getBody());
+    }
+
+    public function testGetStatusCode()
+    {
+        $response = new Response(array('statusCode' => '404'));
+        $this->assertEquals('404', $response->getStatusCode());
+    }
+
+    public function testGetCookie()
+    {
+        $response = new Response(array('cookie' => 'dhauhdiuahisahdhgafydfaytd'));
+        $this->assertEquals('dhauhdiuahisahdhgafydfaytd', $response->getCookie());
+    }
+}
